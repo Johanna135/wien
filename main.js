53daafa70feeb52f8b0040c4e7651c7fc63a042b
Johanna135 <johanna.mascher@student.uibk.ac.at>
2024-04-18 16:12:24 +0200

    layer OpenTopoMap hinzugefügt

--- main.js
+++ main.js
@@ -7,14 +7,19 @@ let stephansdom = {
   title: "Stephansdom",
 };
 
+// alles was mit L. anfangt ist eine Leafletsache
 // Karte initialisieren
 let map = L.map("map").setView([stephansdom.lat, stephansdom.lng], 12);
+// die koordinaten sind in der Variable Stephansdom gespeichert, sie sind keine eigenen variablen --> let stephandsdom, deshlab muss ich auch darauf zugreifen
 
 // BasemapAT Layer mit Leaflet provider plugin als startLayer Variable
+// das ist ein Plugin und wir können die Kartengrundlagen aus diesem Plugin verwenden.
+// ich könnt zum Beispiel auch "BasemapAT.terrain", "BasemapAT.orthofoto" machen dass hab ich an hillshade, orthofoto ... einfach probieren aber haben wir dann in den layers unten
 let startLayer = L.tileLayer.provider("BasemapAT.grau");
 startLayer.addTo(map);
 
 // Hintergrundlayer
+// das sind die layer auf der website auf der Karte, was man auswählen kann
 L.control
   .layers({
     "BasemapAT Grau": startLayer,
@@ -24,13 +29,14 @@ L.control
     "BasemapAT Oberfläche": L.tileLayer.provider("BasemapAT.surface"),
     "BasemapAT Orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
     "BasemapAT Beschriftung": L.tileLayer.provider("BasemapAT.overlay"),
+    "OpenTopoMap": L.tileLayer.provider("OpenTopoMap"), //hab ich dazu getan, man muss ein komma machen am Ende der Zeile
   })
   .addTo(map);
 
 // Marker Stephansdom
 L.marker([stephansdom.lat, stephansdom.lng])
   .addTo(map)
-  .bindPopup(stephansdom.title)
+  .bindPopup(stephansdom.title) //wir verwenden den Title von oben -- als Stephansdom
   .openPopup();
 
 // Maßstab
